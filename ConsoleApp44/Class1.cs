b2e51ff23213729f1c4251b707a0bda4a1be52ad
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-05 21:19:47 +0330

    Add files via upload

---
+++ ConsoleApp44/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Employee
+    {
+        public static int Count { get; private set; }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ++Count;
+            Console.WriteLine("Employee constructor:" + $"{FirstName}{LastName};Count={Count}");
+        }
+    }
+}
---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Employees before instantiation:{Employee.Count}");
+
+            var e1 = new Employee("Susan", "Baker");
+            var e2 = new Employee("Bob", "Blue");
+
+            Console.WriteLine($"\nEmployees after instantiation:{Employee.Count}");
+
+            Console.WriteLine($"\nEmployee 1:{e1.FirstName}{e1.LastName}");
+            Console.WriteLine($"\nEmployee 2:{e2.FirstName}{e2.LastName}");
+            Console.ReadLine();
+
+            e1 = null;
+            e2 = null;
+        }
+    }
+}
